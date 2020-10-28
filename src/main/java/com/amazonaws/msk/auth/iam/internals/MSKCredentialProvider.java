c7327d9be3d733de1ef6e6c6bb01b165e8d98c84
Sayantan Chakravorty <sayantac@amazon.com>
2020-10-28 13:13:01 -0700

    Change MSKCredentialProvider to allow refresh.

--- src/main/java/com/amazonaws/msk/auth/iam/internals/MSKCredentialProvider.java
+++ src/main/java/com/amazonaws/msk/auth/iam/internals/MSKCredentialProvider.java
@@ -8,7 +8,8 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -18,45 +19,40 @@ import java.util.Optional;
  * sasl.jaas.config = com.amazonaws.msk.auth.iam.IAMLoginModule required awsProfileName=<profile name>;
  * The currently supported options are:
  * 1. A particular AWS Credential profile: awsProfileName=<profile name>
- * If no options is provided, the DefaultAWSCredentialsProviderChain is used.
+ * 2. If no options is provided, the DefaultAWSCredentialsProviderChain is used.
  * The DefaultAWSCredentialProviderChain can be pointed to credentials in many different ways:
  * <a href="https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html>Working with AWS Credentials</a>
  * <p>
- * This AWS Credential Provider is meant to be created every time credentials are required and does not refresh.
- * This works for IAMSASLClient since for every authentication a new IAMSaslClient is created resulting in a new
- * MSKCredentialProvider.
  */
 public class MSKCredentialProvider implements AWSCredentialsProvider {
     private static final Logger log = LoggerFactory.getLogger(MSKCredentialProvider.class);
     private static final String AWS_PROFILE_NAME_KEY = "awsProfileName";
-    private final Map<String, ?> options;
     private final AWSCredentialsProvider delegate;
-    private final ProfileCredentialProviderSupplier profileCredentialsProviderSupplier;
 
     public MSKCredentialProvider(Map<String, ?> options) {
-        this(options, (p) -> (new ProfileCredentialsProvider(p)));
+        this(options, getProfileProvider(options));
     }
 
     MSKCredentialProvider(Map<String, ?> options,
-            ProfileCredentialProviderSupplier profileCredentialsProviderSupplier) {
-        this.options = options;
-        this.profileCredentialsProviderSupplier = profileCredentialsProviderSupplier;
-        delegate = new AWSCredentialsProviderChain(getProfileProvider(), new DefaultAWSCredentialsProviderChain());
+            Optional<ProfileCredentialsProvider> profileCredentialsProvider) {
+        List delegateList = new ArrayList<>();
+        profileCredentialsProvider.ifPresent(delegateList::add);
+        delegateList.add(DefaultAWSCredentialsProviderChain.getInstance());
+        delegate = new AWSCredentialsProviderChain(delegateList);
         if (log.isDebugEnabled()) {
             log.debug("Number of options to configure credential provider {}", options.size());
         }
     }
 
-    private AWSCredentialsProvider getProfileProvider() {
-        return new JaasConfigDelegateCredentialProvider(
-                new String[]{AWS_PROFILE_NAME_KEY},
-                (o) -> Optional.ofNullable(o.get(AWS_PROFILE_NAME_KEY)).map(p -> {
-                    if (log.isDebugEnabled()) {
-                        log.debug("Profile name {}", p);
-                    }
-                    return profileCredentialsProviderSupplier.get((String )p);
-                }).orElse(null));
+    private static Optional<ProfileCredentialsProvider> getProfileProvider(Map<String, ?> options) {
+        return Optional.ofNullable(options.get(AWS_PROFILE_NAME_KEY)).map(p -> {
+            if (log.isDebugEnabled()) {
+                log.debug("Profile name {}", p);
+            }
+            return new ProfileCredentialsProvider((String) p);
+        });
     }
+
 
     @Override
     public AWSCredentials getCredentials() {
@@ -65,46 +61,6 @@ public class MSKCredentialProvider implements AWSCredentialsProvider {
 
     @Override
     public void refresh() {
-    }
-
-    /**
-     * This AWS Credential Provider first checks if all the provided option keys are present in the provided JaasConfig.
-     * If they are all present it invokes the config credential provider supplier.
-     */
-    public class JaasConfigDelegateCredentialProvider implements AWSCredentialsProvider {
-        private final String[] optionKeys;
-        private final ConfigCredentialProviderSupplier supplier;
-
-        public JaasConfigDelegateCredentialProvider(String[] optionKeys,
-                ConfigCredentialProviderSupplier supplier) {
-            this.optionKeys = optionKeys;
-            this.supplier = supplier;
-        }
-
-        @Override
-        public AWSCredentials getCredentials() {
-            Optional<String> missingKey = Arrays.stream(optionKeys).filter(k -> !options.containsKey(k)).findFirst();
-            if (!missingKey.isPresent()) {
-                return supplier.getCredentialProvider(options).getCredentials();
-            }
-            return null;
-        }
-
-        @Override
-        public void refresh() {
-        }
-    }
-
-    @FunctionalInterface
-    interface ProfileCredentialProviderSupplier {
-        ProfileCredentialsProvider get(String profile);
-    }
-
-    /**
-     * This interface provides a AWSCredentialsProvider based on JaasConfig options.
-     */
-    @FunctionalInterface
-    interface ConfigCredentialProviderSupplier {
-        AWSCredentialsProvider getCredentialProvider(Map<String, ?> options);
+        delegate.refresh();
     }
 }
